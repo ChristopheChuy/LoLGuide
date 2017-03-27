bcbb156f504f78c013139e14a1bca3b4c26576bd
ChristopheChuy <chuy.christophe@gmail.com>
2017-03-27 19:50:10 +0200

    first commit

---
+++ LoLGuide/CustomViewApp/CustomViewApp/SortCell.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CustomViewApp
+{
+    public partial class SortCell : ViewCell
+    {
+        public SortCell()
+        {
+            InitializeComponent();
+        }
+        public static readonly BindableProperty DescriptionProperty =
+    BindableProperty.Create("Description", typeof(string), typeof(SortCell), "");
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+        public static readonly BindableProperty NomProperty =
+                BindableProperty.Create("Nom", typeof(string), typeof(SortCell), "");
+
+        public string Nom
+        {
+            get { return (string)GetValue(NomProperty); }
+            set { SetValue(NomProperty, value); }
+        }
+        public static readonly BindableProperty ImageProperty =
+              BindableProperty.Create("Image", typeof(string), typeof(SortCell), "");
+
+        public string Image
+        {
+            get { return (string)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+    }
+}
